cd61244df002456158bce66b953e0c1e507c63f1
njang <neo.w.jang@gmail.com>
2018-01-10 22:38:43 -0600

    WIP: calculator logic

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
 
-// Assemble buttons
+// Assemble calculator
 const assemble = () => {
+	let displayElement = $('<input>', {id: 'display', disabled: true})
+	$('.calculator').append(displayElement);
+	$('.calculator').append($('<br>'));
+
 	let buttons = [[7, 8, 9, "&div;"], [4, 5, 6, "&times;"], [1, 2, 3, "&ndash;"], [".", 0, "&equals;", "&plus;"]];
 	for (let i = 0; i < buttons.length; i++) {
 		for (let j = 0; j < buttons[i].length; j++) {
@@ -14,11 +18,40 @@ const assemble = () => {
 }
 
 // Display numbers input by buttons
-let digits = "";
-
+let digits = [];
+let argument = 0;
+let operator = "";
 $(document).ready("#button").click(function(event) {
-  console.log(event.target.value + ' button clicked');
+
+	if (Number.isInteger(Number(event.target.value)) || event.target.value == '.') {
+		digits.push(event.target.value);	
+  } else {
+  	argument = Number(digits.join(''));
+  	digits = [];
+  	switch (event.target.value) {
+	  	case "plus":
+	  		operator = "+";
+	  		// console.log("plus");
+	  		break;
+	  	case "ndash":
+	  		operator = "-"
+	  		// console.log("minus");
+	  		break;
+	  	case "times":
+	  		operator = "x"
+	  		// console.log("times");
+	  		break;
+	  	case "div":
+	  		operator = "/"
+	  		// console.log("div");
+	  		break;
+			default:	
+	  		console.log("equals");
+  	}
+  }
+  console.log(argument, digits.join(''), operator);
+  $("#display").attr("placeholder", digits.join(''));
 });
 
+
 assemble();
-//
